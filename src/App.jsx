185eb2dfc0876272e1d76b664d52fdebfa0f4a8b
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-13 15:06:51 +0530

    render host link added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/auth/login', {
+    const response = await fetch('https://instagramlogin-mongodb.onrender.com', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password }),
