fa6b60643b8b392e102f571501574c3815466cf6
codecraft-sanju <sanjusirvich18@gmail.com>
2025-02-13 20:32:51 +0530

    instagram clone

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,15 @@ app.use(cors());
 
 app.use('/api/auth', authRoutes);
 
-// MongoDB Connection
+// MongoDB Connection (Updated)
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI) 
   .then(() => console.log('✅ MongoDB Connected'))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
+
+app.get((req, res) => {
+  res.send("hello server");
+  })
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
